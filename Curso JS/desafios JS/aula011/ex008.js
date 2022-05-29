8090ba7860169d3abef2862075508dadebc902f6
Jaraí Santos <jaraisantos88@gmail.com>
2022-05-29 13:09:11 -0300

    Aula de JS 29-05

---
+++ Curso JS/desafios JS/aula011/ex008.js
@@ -0,0 +1,6 @@
+var vel = 6.5
+console.log (`A velocidade do seu carro é ${vel}km/h.`)
+if (vel > 60)  {//CONDIÇÃO SIMPLES!
+    console.log (`Vosmecê ultrapassou a velocidade permitida seu burro. MULTADO!!!!!`)
+} 
+console.log (`Dirija usando o cinto de segurança abestado!`)
